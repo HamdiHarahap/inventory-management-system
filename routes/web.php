5671edcb1c5e1392a1da822ec4799046f0391375
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-11-23 10:27:40 +0700

    feat: add middleware and other routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
+
 
 Route::get('/login', [AuthController::class, 'loginView'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard', [
+            'title' => 'Dashboard'
+        ]);
+    })->name('dashboard');
+
+    Route::get('/produk', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/tambah-produk', [ProductController::class, 'create'])->name('product.add');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
