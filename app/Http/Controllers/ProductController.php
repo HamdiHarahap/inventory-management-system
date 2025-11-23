5387ebc43dfee8933b2ecb70b0224c2acf616d90
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-11-23 10:29:43 +0700

    feat: add index and create function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('product.index', [
+            'title' => 'Produk'
+        ]);
+    }
+
+    public function create()
+    {
+        return view('product.add', [
+            "title" => 'Produk'
+        ]);
+    }
 }
