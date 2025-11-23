5810fc02751ab95cfb7cad49346a60936936fc28
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-11-23 10:34:02 +0700

    style: remove register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
-<x-layout>
-    <section class="flex min-h-screen items-center justify-center bg-gray-50">
+<x-auth-layout :title="$title">
+    <section class="flex min-h-screen items-center justify-center ">
         <div class="bg-white shadow-lg border border-gray-200 px-10 py-12 rounded-xl w-full max-w-md">
             
             <h2 class="font-bold text-2xl text-gray-800 text-center">
@@ -15,26 +15,28 @@
             <form action="{{ route('login.post') }}" method="POST" class="mt-8 flex flex-col gap-5">
                 @csrf
 
-                <div class="flex flex-col gap-1">
-                    <label for="email" class="font-semibold text-gray-700">Email</label>
-                    <input 
+                <div class="flex flex-col gap-2">
+                    <div class="flex flex-col gap-1">
+                        <label for="email" class="font-semibold text-gray-700">Email</label>
+                        <input 
                         type="text" 
                         id="email" 
                         name="email"
                         class="border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 rounded-lg px-3 py-2 text-gray-700 transition-all"
-                    >
-                    @error('email') <p class="text-red-600 text-sm">{{ $message }}</p> @enderror
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <label for="password" class="font-semibold text-gray-700">Password</label>
-                    <input 
+                        >
+                        @error('email') <p class="text-red-600 text-sm">{{ $message }}</p> @enderror
+                    </div>
+                    
+                    <div class="flex flex-col gap-1">
+                        <label for="password" class="font-semibold text-gray-700">Password</label>
+                        <input 
                         type="password" 
                         id="password" 
                         name="password"
                         class="border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 rounded-lg px-3 py-2 text-gray-700 transition-all"
-                    >
-                    @error('password') <p class="text-red-600 text-sm">{{ $message }}</p> @enderror
+                        >
+                        @error('password') <p class="text-red-600 text-sm">{{ $message }}</p> @enderror
+                    </div>
                 </div>
 
                 <button 
@@ -44,13 +46,6 @@
                     Login
                 </button>
             </form>
-
-            <p class="mt-5 text-center text-gray-600">
-                Don't have an account?
-                <a href="/register" class="text-indigo-600 hover:text-indigo-800 font-semibold">
-                    Register now
-                </a>
-            </p>
         </div>
     </section>
-</x-layout>
+</x-auth-layout>
