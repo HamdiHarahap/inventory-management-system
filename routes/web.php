03548d1b8e243c11705886aa73fd259282b92341
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-11-24 14:50:17 +0700

    feat: add category CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,13 @@ Route::middleware('auth')->group(function () {
             'title' => 'Dashboard'
         ]);
     })->name('dashboard');
+
+    Route::get('/kategori', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/tambah-kategori', [CategoryController::class, 'create'])->name('category.create');
+    Route::post('/tambah-kategori', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('/edit-kategori/{id}', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::put('/edit-kategori/{id}', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('/kategori/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
 
     Route::get('/produk', [ProductController::class, 'index'])->name('product.index');
     Route::get('/tambah-produk', [ProductController::class, 'create'])->name('product.add');
