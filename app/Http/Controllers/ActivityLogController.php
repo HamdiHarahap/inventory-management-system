655ac7b1b8f903566eee460e8cd333970ec25229
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-11-25 22:06:41 +0700

    feat: implement index method in ActivityLogController

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ActivityLog;
 use Illuminate\Http\Request;
 
 class ActivityLogController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('activity.index', [
+            'title' => 'Aktivitas',
+            'data' => ActivityLog::all()
+        ]);
+    }
 }
