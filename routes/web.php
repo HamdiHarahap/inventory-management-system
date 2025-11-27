42bdc2c58ebcb5bb50ee627438f1905cf193ab47
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-11-27 14:24:20 +0700

    feat: enhance route definitions for customer and supplier management

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\ActivityLogController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BarcodeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,33 +17,45 @@ Route::get('/login', [AuthController::class, 'loginView'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('dashboard', [
-            'title' => 'Dashboard'
-        ]);
-    })->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/tambah-kategori', [CategoryController::class, 'create'])->name('category.create');
+        Route::get('/edit-kategori/{id}', [CategoryController::class, 'edit'])->name('category.edit');
+        Route::post('/tambah-kategori', [CategoryController::class, 'store'])->name('category.store');
+        Route::put('/edit-kategori/{id}', [CategoryController::class, 'update'])->name('category.update');
+        Route::delete('/kategori/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+
+        Route::get('/tambah-produk', [ProductController::class, 'create'])->name('product.create');
+        Route::post('/tambah-produk', [ProductController::class, 'store'])->name('product.store');
+        Route::get('/edit-produk/{id}', [ProductController::class, 'edit'])->name('product.edit');
+        Route::put('/edit-produk/{id}', [ProductController::class, 'update'])->name('product.update');
+        Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+
+        Route::get('/tambah-pengguna', [UserController::class, 'create'])->name('user.create');
+        Route::post('/tambah-pengguna', [UserController::class, 'store'])->name('user.store');
+        Route::get('/edit-pengguna/{id}', [UserController::class, 'edit'])->name('user.edit');
+        Route::put('/edit-pengguna/{id}', [UserController::class, 'update'])->name('user.update');
+        Route::delete('/pengguna/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+        Route::get('/tambah-customer', [CustomerController::class, 'create'])->name('customer.create');
+        Route::post('/tambah-customer', [CustomerController::class, 'store'])->name('customer.store');
+        Route::get('/edit-customer/{id}', [CustomerController::class, 'edit'])->name('customer.edit');
+        Route::put('/edit-customer/{id}', [CustomerController::class, 'update'])->name('customer.update');
+        Route::delete('/customer/{id}', [CustomerController::class, 'destroy'])->name('customer.destroy');
+
+        Route::get('/tambah-supplier', [SupplierController::class, 'create'])->name('supplier.create');
+        Route::post('/tambah-supplier', [SupplierController::class, 'store'])->name('supplier.store');
+        Route::get('/edit-supplier/{id}', [SupplierController::class, 'edit'])->name('supplier.edit');
+        Route::put('/edit-supplier/{id}', [SupplierController::class, 'update'])->name('supplier.update');
+        Route::delete('/supplier/{id}', [SupplierController::class, 'destroy'])->name('supplier.destroy');
+    });
 
     Route::get('/kategori', [CategoryController::class, 'index'])->name('category.index');
-    Route::get('/tambah-kategori', [CategoryController::class, 'create'])->name('category.create');
-    Route::post('/tambah-kategori', [CategoryController::class, 'store'])->name('category.store');
-    Route::get('/edit-kategori/{id}', [CategoryController::class, 'edit'])->name('category.edit');
-    Route::put('/edit-kategori/{id}', [CategoryController::class, 'update'])->name('category.update');
-    Route::delete('/kategori/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
-
     Route::get('/produk', [ProductController::class, 'index'])->name('product.index');
-    Route::get('/tambah-produk', [ProductController::class, 'create'])->name('product.create');
-    Route::post('/tambah-produk', [ProductController::class, 'store'])->name('product.store');
-    Route::get('/edit-produk/{id}', [ProductController::class, 'edit'])->name('product.edit');
-    Route::put('/edit-produk/{id}', [ProductController::class, 'update'])->name('product.update');
-    Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
-
     Route::get('/pengguna', [UserController::class, 'index'])->name('user.index');
-    Route::get('/tambah-pengguna', [UserController::class, 'create'])->name('user.create');
-    Route::post('/tambah-pengguna', [UserController::class, 'store'])->name('user.store');
-    Route::get('/edit-pengguna/{id}', [UserController::class, 'edit'])->name('user.edit');
-    Route::put('/edit-pengguna/{id}', [UserController::class, 'update'])->name('user.update');
-    Route::delete('/pengguna/{id}', [UserController::class, 'destroy'])->name('user.destroy');
-
+    Route::get('/customer', [CustomerController::class, 'index'])->name('customer.index');
+    Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier.index');
     Route::get('/aktivitas', [ActivityLogController::class, 'index'])->name('activity.index');
 
     Route::get('/barcode/{code}', [BarcodeController::class, 'generateBarcode']);
