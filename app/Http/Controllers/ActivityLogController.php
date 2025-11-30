301a7fef9ad5064f79c2f07f4397f4086a5cecd7
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-11-30 21:40:04 +0700

    feat: enhance activity log index with date and user filtering

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\ActivityLog;
 use Illuminate\Http\Request;
 
 class ActivityLogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $date = $request->date;
+        $userId = $request->user;
+
+        $data = ActivityLog::when($date, function ($query) use ($date) {
+                $query->whereDate('created_at', $date);
+            })
+            ->when($userId, function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
+
         return view('activity.index', [
             'title' => 'Aktivitas',
-            'data' => ActivityLog::all()
+            'data' => $data,
+            'users' => User::orderBy('created_at')->where('role', '!=', 'manager')->get()
         ]);
     }
+
 }
